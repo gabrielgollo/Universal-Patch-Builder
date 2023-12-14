08db72ba89423395e13cc13bd8ca27ba52417493
GabrielGollo <gabrielsantosgollo@gmail.com>
2023-12-14 14:09:05 -0300

    Adicionar arquivos de projeto.

---
+++ UniversalPatch Builder/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniversalPatch_Builder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PatcherGenerator patcher;
+        public MainWindow()
+        {
+            InitializeComponent();
+            patcher = new PatcherGenerator();
+        }
+
+        private void SelecionarPasta_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    System.Windows.MessageBox.Show(dialog.SelectedPath);
+                    caminhoDaPastaTextBox.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void ListFiles(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string folderPath = caminhoDaPastaTextBox.Text;
+
+                if (folderPath.Length > 10)
+                {
+
+                    filesListBox.Items.Clear(); 
+                    List<string> files = patcher.ReadAllFiles(folderPath);
+                    // string fileStr = string.Join(Environment.NewLine, files);
+
+                    foreach(string file in files) {
+                        filesListBox.Items.Add(file);
+                    }
+
+                }
+                else
+                {
+                    throw new Exception("Selecione um caminho antes");
+                }
+            } catch (Exception error)
+            {
+                System.Windows.MessageBox.Show("Error: " + error.Message);
+            }
+
+        }
+
+        private void CalcularMD5_Click(object sender, RoutedEventArgs e)
+        {
+            try {
+                // Obtemos os caminhos dos arquivos da TextBox
+                List<string> filePathList = new List<string>();
+
+                foreach (object fileItem in filesListBox.Items)
+                {
+                    filePathList.Add(fileItem.ToString());
+                }
+
+                
+                // Processamos os arquivos e obtemos informações
+                List <FileInfo> filesInfos = patcher.ProcessFiles(filePathList);
+
+                // Exibimos os resultados na nova TextBox
+                md5ResultsTextBox.Text = "Resultados do MD5:\n";
+
+                foreach (FileInfo fileInfo in filesInfos)
+                {
+                    md5ResultsTextBox.Text += $"{fileInfo.filePath}: {fileInfo.MD5}\n";
+                }
+            } catch(Exception error)
+            {
+                System.Windows.MessageBox.Show("An error has occurred!" + error.Message);
+            }
+            
+        }
+    }
+}
---
+++ UniversalPatch Builder/src/PatcherGenerator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+
+class FileInfo
+{
+    public string filePath;
+    public string MD5;
+
+    public FileInfo(string _filePath, string _MD5)
+    {
+        filePath = _filePath;
+        MD5 = _MD5;
+    }
+}
+
+namespace UniversalPatch_Builder
+{
+    class PatcherGenerator
+    {
+        public List<string> ReadAllFiles(string folderPath)
+        {
+            List<string> fileList = new List<string>();
+            try
+            {
+                foreach (string subFolderPath in Directory.GetDirectories(folderPath))
+                {
+                    List<string> subFiles = ReadAllFiles(subFolderPath);
+                    foreach(string subFile in subFiles)
+                    {
+                        fileList.Add(subFile);
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+
+            foreach (string filePath in Directory.GetFiles(folderPath))
+            {
+                fileList.Add(filePath);
+            }
+
+            return fileList;
+        }
+
+        public List<FileInfo> ProcessFiles(List<string> filePathList)
+        {
+            List<FileInfo> filesInfos = new List<FileInfo>();
+            foreach (string filePath in filePathList)
+            {
+                string MD5 = CalculateMD5(filePath.Replace("\r", "").Replace("\n", ""));
+                FileInfo fileInfo = new FileInfo(filePath, MD5);
+
+                filesInfos.Add(fileInfo);
+            }
+
+            return filesInfos;
+        }
+
+        private string CalculateMD5(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    byte[] hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLower();
+                }
+            }
+        }
+    }
+}
